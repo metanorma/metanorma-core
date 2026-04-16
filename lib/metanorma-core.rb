833f1fb064b2933f9141735d7e0e1d7ef4a10a26
Nick Nicholas <opoudjis@gmail.com>
2026-04-16 23:10:37 +1000

    init

---
+++ lib/metanorma-core.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "metanorma/core/version"
+require "asciidoctor"
+require "metanorma/config/config"
+require "metanorma/util/util"
+require "metanorma/input"
+require "metanorma/processor/processor"
+require "metanorma/registry/registry"
+require "metanorma/asciidoctor_extensions"
+
+module Metanorma
+end
