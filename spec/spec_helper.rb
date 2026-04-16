4bea2770ad265a8f80de2e64ae1c7cd4009df4d7
Nick Nicholas <opoudjis@gmail.com>
2026-04-16 23:10:48 +1000

    init

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+require "bundler/setup"
+require "metanorma-core"
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
