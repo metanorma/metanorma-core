927a7c41dd2bd681b28e271d35a0f62088928dce
Nick Nicholas <opoudjis@gmail.com>
2026-04-20 21:15:44 +1000

    move isodoc loader to metanorma-core: https://github.com/metanorma/metanorma/issues/548

--- lib/metanorma-core.rb
+++ lib/metanorma-core.rb
@@ -8,6 +8,8 @@ require "metanorma/input"
 require "metanorma/processor/processor"
 require "metanorma/registry/registry"
 require "metanorma/asciidoctor_extensions"
+require "metanorma/core/flavor_loader"
+require "metanorma/core/isodoc"
 
 module Metanorma
 end
